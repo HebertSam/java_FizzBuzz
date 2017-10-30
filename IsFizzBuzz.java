e4cd74b403ea519f6b417ef314e509844d7d7d43
unknown <hebertsa07@gmail.com>
2017-10-30 10:37:18 -0400

    adding new files

---
+++ IsFizzBuzz.java
@@ -0,0 +1,9 @@
+public class IsFizzBuzz{
+    public static void main(String [] args){
+        FizzBuzz fizz = new FizzBuzz();
+        System.out.println(fizz.fizzBuzz(3));
+        System.out.println(fizz.fizzBuzz(5));
+        System.out.println(fizz.fizzBuzz(15));
+        System.out.println(fizz.fizzBuzz(2));
+    }
+}
